fae980b26d6524a2c557ec25531691437e02ae2f
Petrov Kirill <ottokirik@gmail.com>
2021-10-17 01:18:19 +0300

    Init & created useInput hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useInput } from 'hooks/useInput';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const username = useInput('');
+    const email = useInput('');
+
+    return (
+        <div>
+            <form>
+                <input {...email} type="email" name="email" placeholder="Email" />
+                <input {...username} type="text" name="username" placeholder="Username" />
+            </form>
+            <div>{username.value}</div>
+            <div>{email.value}</div>
+        </div>
+    );
 }
 
 export default App;
