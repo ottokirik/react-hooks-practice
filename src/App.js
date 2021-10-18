89489e496aae17da78fd1428369657f0b20b0b59
Petrov Kirill <ottokirik@gmail.com>
2021-10-18 14:32:52 +0300

    Created useRequest hook

--- src/App.js
+++ src/App.js
@@ -1,39 +1,53 @@
 // import { List } from 'components/List';
 // import { useInput } from 'hooks/useInput';
 
-import { useDebounce } from 'hooks/useDebounce';
-import { useState } from 'react';
+// import { useDebounce } from 'hooks/useDebounce';
+import { useRequest } from 'hooks/useRequest';
+// import { useState } from 'react';
 
 function App() {
     // const username = useInput('');
     // const email = useInput('');
-    const [todos, setTodos] = useState([]);
-    const [query, setQuery] = useState('');
 
-    const debouncedFetch = useDebounce(serach, 500);
+    const [todos, loading, error] = useRequest(fetchTodos);
+    // const [query, setQuery] = useState('');
 
-    function serach(query) {
-        fetch(`https://jsonplaceholder.typicode.com/todos?title=${query}`)
-            .then((res) => {
-                if (!res.ok) throw new Error('Нет данных');
-                return res.json();
-            })
-            .then((json) => {
-                setTodos((prev) => [...prev, ...json]);
-            })
-            .catch((e) => console.log(e));
+    // const debouncedFetch = useDebounce(serach, 500);
+
+    // function serach(query) {
+    //     fetch(`https://jsonplaceholder.typicode.com/todos?title=${query}`)
+    //         .then((res) => {
+    //             if (!res.ok) throw new Error('Нет данных');
+    //             return res.json();
+    //         })
+    //         .then((json) => {
+    //             setTodos((prev) => [...prev, ...json]);
+    //         })
+    //         .catch((e) => console.log(e));
+    // }
+
+    function fetchTodos() {
+        return fetch(`https://jsonplaceholder.typicode.com/todos`);
+    }
+
+    if (loading) {
+        return <h1>Идет загрузка...</h1>;
+    }
+
+    if (error) {
+        return <h1>Произошла ошибка при загрузке данных</h1>;
     }
 
     return (
         <div>
-            <input
+            {/* <input
                 type="text"
                 value={query}
                 onChange={(e) => {
                     setQuery(e.target.value);
                     debouncedFetch(query);
                 }}
-            />
+            /> */}
             {/* <form>
                 <input {...email} type="email" name="email" placeholder="Email" />
                 <input {...username} type="text" name="username" placeholder="Username" />
@@ -41,11 +55,12 @@ function App() {
             <div>{username.value}</div>
             <div>{email.value}</div> */}
             {/* <List /> */}
-            {todos.map((todo) => (
-                <div style={{ padding: 30, borderBottom: '1px solid #ccc' }} key={todo.id}>
-                    {todo.id} - {todo.title}
-                </div>
-            ))}
+            {todos &&
+                todos.map((todo) => (
+                    <div style={{ padding: 30, borderBottom: '1px solid #ccc' }} key={todo.id}>
+                        {todo.id} - {todo.title}
+                    </div>
+                ))}
         </div>
     );
 }
---
+++ src/hooks/useRequest.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+
+export const useRequest = (request) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        setLoading(true);
+        request()
+            .then((response) => response.json())
+            .then((json) => setData(json))
+            .catch((error) => setError(error))
+            .finally(() => setLoading(false));
+    }, []);
+
+    return [data, loading, error];
+};
